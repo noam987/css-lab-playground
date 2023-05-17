5ddf253ad26d02627eda0eb753adad88df3c018b
Noam987 <noam@arie11.com>
2023-05-17 16:54:14 -0400

    initial commit

---
+++ src/localize_objects.py
@@ -0,0 +1,13 @@
+def localize_objects(path):
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+    with open(path, 'rb') as image_file:
+        content = image_file.read()
+    image = vision.Image(content = content)
+    objects = client.object_localization(image=image).localized_object_annotations
+    print(f'Number of objects found: {len(objects)}')
+    for object_ in objects:
+        print(f'\n{object_.name} (confidence: {object_.score})')
+        print('Normalized bounding polygon vertices: ')
+        for vertex in object_.bounding_poly.normalized_vertices:
+            print(f' - ({vertex.x}, {vertex.y})')
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from localize_objects import localize_objects
+
+
+
+def main():
+    localize_objects('test_image_1.jpg')
+
+if __name__ == '__main__':
+    main()
