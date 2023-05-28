10ec767bdf32aa3cd90db0a6cff88b8af4faf9f9
noam987 <noam@arie11.com>
2023-05-28 14:10:48 -0400

    Added glob to automatically parse all the files

--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
 from localize_objects import localize_objects
 from detect_faces import detect_faces
 import csv
+import glob
+
 header = ['file_path', 'face_number', 'headware_likelyhood', 'roll_angle', 'pan_angle', 'tilt_angle', 'x1', 'y1', 'x2', 'y2', 'x3', 'y3', 'x4', 'y4']
 def main():
-    with open('test.csv', 'w', encoding='UTF8', newline='') as f:
+    with open('test2.csv', 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(header)
-        for i in range(1, 7):
-            print(f'Image Number {i}:')
-            detect_faces(f'images/test_image_{i}.jpg', writer)
+        for path in glob.iglob('images/*.jpg'):
+            detect_faces(path, writer)
             
 if __name__ == '__main__':
     main()
